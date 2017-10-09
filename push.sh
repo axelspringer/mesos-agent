97e1056be3fc5dff6cea84526f1a58fbfc1f6d85
Döll, Sebastian <sebastian.doell@axelspringer.de>
2017-10-09 12:53:37 +0200

    ~ v1.4.0

--- push.sh
+++ push.sh
@@ -9,12 +9,12 @@ push() {
 
 # base
   echo
-  echo Pushing pixelmilk/mesos-${ROLE}:${TAG}
-  docker push pixelmilk/mesos-${ROLE}:${TAG} || exit $?
+  echo Pushing axelspringer/mesos-${ROLE}:${TAG}
+  docker push axelspringer/mesos-${ROLE}:${TAG} || exit $?
 }
 
 # login docker before push
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 #    Mesos version
-push "1.3.1"
+push "1.4.0"
